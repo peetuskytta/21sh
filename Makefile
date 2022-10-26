ca010ee751e97daa3357d4c933b4cb1e01aa68c7
pskytta <77061872+peetuskytta@users.noreply.github.com>
2022-10-26 16:10:10 +0300

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,47 @@
+#Compiler and compiler flags
+CC := gcc
+#FLAGS := -Wall -Wextra -Werror -g -fsanitize=address
+FLAGS := -Wall -Wextra -Werror
+
+#Color scheme
+WHITE = \033[37m
+RESET = \033[0m
+RED = \033[31m
+BOLD = \033[1m
+
+#Target Binary Program
+NAME := minishell
+
+SOURCES :=
+
+OBJ := $(SOURCES:.c=.o)
+
+LIBFT_M := make -s -C srcs/libft
+LIBFT_A := srcs/libft/libft.a
+HEADER := includes/21sh.h
+
+all: $(NAME)
+
+$(NAME): $(OBJ) $(LIBFT_A)
+	@echo "$(BOLD)$(WHITE)$(NAME) Compilation done$(RESET)"
+	@$(CC) $(FLAGS) $(OBJ) $(LIBFT_A) -o $(NAME)
+
+%.o: %.c
+	$(CC) $(FLAGS) -c $< -o $@ -I includes/
+
+$(LIBFT_A):
+	@make -C libft/
+
+clean:
+	@make -C libft clean
+	@rm -f $(OBJ)
+	@echo "$(BOLD)$(RED)OBJ deleted$(RESET)"
+
+fclean: clean
+	@make -C libft fclean
+	@rm -f $(NAME)
+	@echo "$(BOLD)$(RED)$(NAME) deleted$(RESET)"
+
+re: fclean all
+
+.PHONY: all, clean, fclean, re
---
+++ inlcudes/shell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shell.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskytta <pskytta@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 12:53:53 by zraunio           #+#    #+#             */
+/*   Updated: 2022/10/26 16:03:54 by pskytta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef SHELL_H
+# define SHELL_H
+# include "../libft/includes/libft.h"
+
+
+typedef struct s_shell
+{
+	struct	pipe;
+	
+}	t_shell;
+
+typedef struct s_cmd_pipe
+{
+	/* data */
+}	t_pipe;
+
