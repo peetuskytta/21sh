246cd669d2768aded41fd2f5b03bb4a4575bb967
Rori Ivanovo <zraunio@student.hive.fi>
2022-11-12 14:02:52 +0200

    clean

--- srcs/rawmode/kill_mode.c
+++ srcs/rawmode/kill_mode.c
@@ -6,7 +6,7 @@
 /*   By: zraunio <zraunio@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 13:28:31 by zraunio           #+#    #+#             */
-/*   Updated: 2022/11/12 13:01:06 by zraunio          ###   ########.fr       */
+/*   Updated: 2022/11/12 14:00:19 by zraunio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void kill_mode(const char *str, t_shell *shell)
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		ft_putendl_fd(str, STDERR_FILENO);
-		// free(shell);
 		exit(EXIT_SUCCESS);
 	}
 }
