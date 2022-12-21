880a431c199dbc14d99fc34f3bfcc8e2defa98bd
peetuskytta <peetu.skytta@gmail.com>
2022-12-21 12:41:59 +0200

    peetu changes on linux

--- srcs/parse/parser.c
+++ srcs/parse/parser.c
@@ -14,21 +14,21 @@
 
 t_tok	*parser(t_shell *shell)
 {
-	//t_tok	*temp;
+	t_tok	*temp;
 	t_lex	list;
 
 	ft_memset(&list, 0, sizeof(t_lex));
 	token_list_build(shell->cmd_line, shell->cmd_idx, &list);
-	//temp = list.token_list;
+	temp = list.token_list;
 	NL;
-//	int i = 0;
-//	while (temp != NULL)
-//	{
-//		if (temp->str)
-//			ft_printf("token[%d] type [%d] = %s", i++, temp->type, temp->str);
-//		temp = temp->next;
-//		if (temp != NULL)
-//			NL;
-//	}
+	int i = 0;
+	while (temp != NULL)
+	{
+		if (temp->str)
+			ft_printf("token[%d] type [%d] = %s", i++, temp->type, temp->str);
+		temp = temp->next;
+		if (temp != NULL)
+			NL;
+	}
 	return (list.token_list);
 }
