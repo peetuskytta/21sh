f51fd10cb96fdf859c9317c34d11d79674da2b26
Zornitsa Raunio <zraunio@c2r2p8.hive.fi>
2023-01-05 17:38:40 +0200

    delete chr on first line works better with cmd_idx vs loc

--- srcs/input/cmd_line.c
+++ srcs/input/cmd_line.c
@@ -6,7 +6,7 @@
 /*   By: zraunio <zraunio@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 13:32:38 by zraunio           #+#    #+#             */
-/*   Updated: 2023/01/04 17:02:24 by zraunio          ###   ########.fr       */
+/*   Updated: 2023/01/05 16:56:25 by zraunio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ void	cmd_line(t_shell *shell, char c)
 	int	i;
 
 	i = ft_strilen(shell->cmd_line);
-	if (i < shell->cmd_idx)
-	{
+	// if (i < shell->cmd_idx)
+	// {
 		shell->cmd_line[i] = c;
 		shell->cmd_idx++;
 		shell->window.loc += 1;
-	}
-	else
-	{
-		shell->cmd_line[shell->cmd_idx] = c;
-		shell->cmd_idx++;
-		shell->window.loc += 1;
-	}
+	// }
+	// else
+	// {
+	// 	shell->cmd_line[shell->cmd_idx] = c;
+	// 	shell->cmd_idx++;
+	// 	shell->window.loc += 1;
+	// }
 }
