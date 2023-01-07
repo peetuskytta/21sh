5c2cd1f611d7563673d2c4ca49ef984bf1271454
Rori Ivanovo <zraunio@student.hive.fi>
2023-01-07 15:26:30 +0200

    cmd lien reprint rebuilding blocks

--- srcs/input/cmd_line_reprint.c
+++ srcs/input/cmd_line_reprint.c
@@ -6,7 +6,7 @@
 /*   By: zraunio <zraunio@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:36:22 by zraunio           #+#    #+#             */
-/*   Updated: 2023/01/07 15:14:21 by zraunio          ###   ########.fr       */
+/*   Updated: 2023/01/07 15:24:22 by zraunio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	cmd_line_reprint(t_shell *shell, t_win *window, char c)
 {
 	int	x;
 
+	x = shell->prmpt_len;
 	if (window->idx == 0)
 	{
-		x = shell->prmpt_len;
 		cursor_reset_line(window, x);
 		ft_putstr_fd(shell->cmd_line, STDIN_FILENO);
 		stdin_char(c);
@@ -26,9 +26,11 @@ void	cmd_line_reprint(t_shell *shell, t_win *window, char c)
 	}
 	else
 	{
-		x = 0;
-		cursor_reset_line(window, x);
-		ft_putstr_fd(window->row_idx[window->idx], STDIN_FILENO);
+		// cursor_reset_line(window, 0);
+		tputs(tgoto(tgetstr("cm", NULL), 3, window->current_row - window->idx - 1), 1, stdin_char);
+		tputs(tgoto(tgetstr("cd", NULL), 3, window->current_row - window->idx - 1), 1, stdin_char);
+		ft_putstr_fd(shell->cmd_line, STDIN_FILENO);
+		//ft_putstr_fd(window->row_idx[window->idx], STDIN_FILENO);
 		stdin_char(c);
 		input_rev_cmd(shell);
 	}
