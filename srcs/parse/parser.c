41e06c872f70abddd410ce7c0491b30846368ff4
pskytta <pskytta@student.hive.fi>
2023-01-19 15:13:49 +0200

    Did some norm cleaning in parsing functions.

--- srcs/parse/parser.c
+++ srcs/parse/parser.c
@@ -13,25 +13,25 @@
 #include "../../includes/parse.h"
 
 /*DELETE BEFORE SUBMIT*/
-void	token_list_print(t_tok *token)
-{
-	t_tok	*temp;
-	int		i;
+// void	token_list_print(t_tok *token)
+// {
+// 	t_tok	*temp;
+// 	int		i;
 
-	i = 0;
-	temp = token;
-	NL;
-	while (temp != NULL)
-	{
-		if (temp->str)
-			ft_printf("token[%d] type [%d] = %s", i++, temp->type, temp->str);
-		temp = temp->next;
-		if (temp != NULL)
-			NL;
-	}
-	if (temp == NULL)
-		ft_printf("\ntoken[%d] (NULL)\n", i);
-}
+// 	i = 0;
+// 	temp = token;
+// 	NL;
+// 	while (temp != NULL)
+// 	{
+// 		if (temp->str)
+// 			ft_printf("token[%d] type [%d] = %s", i++, temp->type, temp->str);
+// 		temp = temp->next;
+// 		if (temp != NULL)
+// 			NL;
+// 	}
+// 	if (temp == NULL)
+// 		ft_printf("\ntoken[%d] (NULL)\n", i);
+// }
 
 t_tok	*parser(t_shell *shell)
 {
@@ -41,11 +41,11 @@ t_tok	*parser(t_shell *shell)
 	shell->cmd_idx *= 2;
 	if (shell->cmd_idx > 0)
 		token_list_build(shell->cmd_line, shell->cmd_idx, &list);
-	t_tok	*temp = list.token_list;
-	token_list_print(temp);
 	parse_errors(&list.token_list);
 	return (list.token_list);
 }
+	// t_tok	*temp = list.token_list;
+	// token_list_print(temp);
 
 // 2>&1 ---> redirect STDERR to STDOUT, redirects it to the same location
 // 1>&2 ---> redirect STDOUT to STDERR, redirects it to the same location
