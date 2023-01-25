2613e95577326905fc9584274f083c8e9af8e7d9
peetuskytta <peetu.skytta@gmail.com>
2023-01-25 11:22:36 +0200

    env -i works.

--- srcs/builtins/builtin_env.c
+++ srcs/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: pskytta <pskytta@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 10:20:57 by zraunio           #+#    #+#             */
-/*   Updated: 2023/01/25 09:48:08 by pskytta          ###   ########.fr       */
+/*   Updated: 2023/01/25 11:21:17 by pskytta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,11 @@ void	swap_char_ptr(char **first, char **second) //maybe a function to add to lib
 	*second = temp;
 }
 
-static bool is_command(t_exec *new, int i)
+static bool is_command(t_exec *data, int i)
 {
-	while (new->args[i])
+	while (data->args[i])
 	{
-		if (!ft_strchr(new->args[i], '='))
+		if (!ft_strchr(data->args[i], '='))
 			return (true);
 		i++;
 	}
@@ -53,69 +53,93 @@ static bool is_command(t_exec *new, int i)
 
 }*/
 
-void	execute_env(t_exec new, char **env_cpy)
+static void	execute_env(t_shell *shell, t_exec new, char **env_cpy)
 {
 	char	*bin_path;
 
 	bin_path = NULL;
-	bin_path = exec_find_binary(exec_fetch_path_var(env_cpy), new.cmd);
-	if (redirection_loop(&new) && exec_binary_check(bin_path, new))
-		exec_cmd(new, bin_path, env_cpy);
-	ft_strdel((void *)&bin_path);
-}
-
-void	env_args_renew(t_exec new, int i)
-{
-	(void)new;
-	(void)i;
-}
-
-static bool	empty_env_i_flag(t_exec new)
-{
-	int		i;
-
-	i = 1;
-	ft_putchar_fd('\n', STDOUT_FILENO);
-	while (new.args[i])
+	if (is_builtin(new.cmd))
 	{
-		if (ft_strchr(new.args[i], '='))
-			ft_print_fd(STDOUT_FILENO, "%s\n", new.args[i]);
-		ft_strdel(&new.args[i]);
+		builtin_execute(shell, new, env_cpy);
+	}
+	else
+	{
+		bin_path = exec_find_binary(exec_fetch_path_var(env_cpy), new.cmd);
+		if (redirection_loop(&new) && exec_binary_check(bin_path, new))
+			exec_cmd(new, bin_path, env_cpy);
+		ft_strdel((void *)&bin_path);
+	}
+}
+
+static bool	env_cdm(t_shell *shell, t_exec new, t_exec data, int i)
+{
+	int		idx;
+	int		env_idx;
+	char	**env_cpy;
+
+	env_idx = 0;
+	idx = 0;
+	env_cpy = (char **)ft_memalloc(sizeof(char *) * (MAX_REDIR - 1));
+	while (data.args[i])
+	{
+		if (!ft_strchr(data.args[i], '='))
+		{
+			new.args[idx] = ft_strdup(data.args[i]);
+			idx++;
+		}
+		if (ft_strchr(data.args[i], '='))
+		{
+			env_cpy[env_idx] = ft_strdup(data.args[i]);
+			env_idx++;
+		}
+		ft_strdel(&data.args[i]);
+		i++;
+	}
+/* 	i = 0;
+	NL;
+	while (new.args[i])
+		ft_putendl(new.args[i++]);
+ 	i = 0;
+	NL;
+	while (env_cpy[i])
+		ft_putendl(env_cpy[i++]); */
+	new.cmd = ft_strdup(new.args[0]);
+	execute_env(shell, new, env_cpy);
+	ft_arr_free((void *)&env_cpy);
+	i = 0;
+	ft_strdel(&data.args[1]);
+	while (new.args[i])
+		ft_strdel(&new.args[i++]);
+	ft_strdel(&new.cmd);
+	return (true);
+}
+
+static bool	env_i_no_cmd(t_exec data, int i)
+{
+	ft_putchar_fd('\n', STDOUT_FILENO);
+	while (data.args[i])
+	{
+		if (ft_strchr(data.args[i], '='))
+			ft_print_fd(STDOUT_FILENO, "%s\n", data.args[i]);
+		ft_strdel(&data.args[i]);
 		i++;
 	}
 	return (false);
 }
 
-static bool	env_temp_empty(t_exec data)
+static bool	env_temp_i(t_shell *shell, t_exec data)
 {
 	t_exec	new;
 
 	if (data.args[2] == NULL)
 		return (false);
 	ft_memset(&new, 0, sizeof(new));
-	ft_memcpy(&new, &data, sizeof(t_exec));
-	if (is_command(&new, 2))
-	{
-		return (0);
-	}
+	if (is_command(&data, 2))
+		return (env_cdm(shell, new, data, 2));
 	else
-		return (empty_env_i_flag(new));
-	/* {
-		env_args_renew(data, 2);
-
-		ft_putendl(data.cmd);
-		DB;
-		int i = 0;
-		while (data.args[i])
-			ft_putendl(data.args[i++]);
-		DB;
-		i = 0;
-		while (env_cpy[i])
-			ft_putendl(env_cpy[i++]);
-
-		real_exec(new);
-		return (true);
-	} */
+	{
+		return (env_i_no_cmd(data, 1));
+	}
 	return (false);
 }
 
@@ -133,7 +157,7 @@ bool	builtin_env(t_shell *shell, t_exec data, char **env_cpy)
 	flg = env_flag(data.args);
 	(void)shell;
 	if (flg == true)
-		return (env_temp_empty(data));
+		return (env_temp_i(shell, data));
 	if (data.args[1] != NULL && flg == false)
 	{
 		//env_temp_addvar(data.args, env_cpy);
