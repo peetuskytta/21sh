cd84a069d21d55ceae2f50eeceb886f83c765801
Rori Ivanovo <zraunio@student.hive.fi>
2023-02-01 14:28:11 +0200

    some errors

--- srcs/execute/exec_binary_check.c
+++ srcs/execute/exec_binary_check.c
@@ -18,7 +18,7 @@ bool	exec_binary_check(char **bin_path, t_exec data)
 		return (false);
 	if (ft_strequ(data.args[0], "./21sh"))
 		*bin_path = ft_strdup(data.args[0]);
-	if (access(*bin_path, F_OK) == -1)
+	if (access(*bin_path, F_OK) == -1 || *bin_path == NULL)
 	{
 		ft_putstr_fd("shell: ", STDERR_FILENO);
 		ft_putstr_fd(data.cmd, STDERR_FILENO);
