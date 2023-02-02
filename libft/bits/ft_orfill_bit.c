e5ed6960eaed5bc3132f237a301309e8f8f82cde
Zornitsa Raunio <zraunio@c2r6p3.hive.fi>
2023-02-02 16:20:23 +0200

    stuff

--- libft/bits/ft_orfill_bit.c
+++ libft/bits/ft_orfill_bit.c
@@ -6,7 +6,7 @@
 /*   By: zraunio <zraunio@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:10:16 by zraunio           #+#    #+#             */
-/*   Updated: 2021/03/17 12:45:08 by zraunio          ###   ########.fr       */
+/*   Updated: 2023/02/02 15:24:03 by zraunio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ size_t	ft_orfill_bit(size_t nb, size_t *arr)
 	return (nb);
 }
 /*
+** comment
 ** adds bits using | operator
 ** int array last value MUST be 0;
 */
