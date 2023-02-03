298aacc2667f3d6d7e69403a2e91748f9e43c080
pskytta <pskytta@student.hive.fi>
2023-02-03 12:51:24 +0200

    eval testing

--- srcs/execute/exec_clear_data.c
+++ srcs/execute/exec_clear_data.c
@@ -6,7 +6,7 @@
 /*   By: pskytta <pskytta@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 15:26:16 by pskytta           #+#    #+#             */
-/*   Updated: 2023/01/29 14:32:28 by pskytta          ###   ########.fr       */
+/*   Updated: 2023/02/03 12:32:03 by pskytta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,15 @@ void	exec_clear_data(t_exec *data)
 		ft_memdel((void *)&data->args[i++]);
 	if (data->cmd)
 		ft_memdel((void *)&data->cmd);
-	if (data->redir[0].file != NULL)
+	if (data->redir[0].file != NULL || data->redir[0].file_in != NULL)
 	{
 		i = 0;
-		while (data->redir[i].file != NULL)
+		while (data->redir[i].file != NULL || data->redir[i].file_in != NULL)
 		{
 			if (data->redir[i].file)
 				ft_strdel((void *)&data->redir[i].file);
+			if (data->redir[i].file_in)
+				ft_strdel((void *)&data->redir[i].file_in);
 			data->redir[i].type = -1;
 			data->redir[i].fd_in = -1;
 			data->redir[i].agre = -1;
